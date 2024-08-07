38f85b22d7b47c0be92d1dc2dddd134ec7db3aec
Brandon Crotty <brandisong00@gmail.com>
2024-08-07 13:08:03 +1000

    Added functionality to sum value of an inventory. Formatted str() for both products and inventories.

--- Classes/1_Product_Inventory/Python/product_inventory.py
+++ Classes/1_Product_Inventory/Python/product_inventory.py
@@ -8,28 +8,63 @@ Then create an *inventory* class which keeps track of various products
 
 # Product class: has a price, id, and stock-on-hand
 class Product:
-    def __init__(self, price, id, soh):
+    def __init__(self, name, price, id, soh):
+        self.name = str(name)
         self.price = price
         self.id = id
         self.soh = soh
 
+    
+    def __str__(self):
+        return f"{self.name} ({self.id}) | SOH:{self.soh} | ${self.price}"
+
 
 # Inventory: represents a store's inventory, contains a list of product instances
 class Inventory:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = str(name)
         self.products = []
+
+    
+    def __str__(self):
+        return self.name
     
 
     def AddProduct(self, product):
         if product not in self.products:
             self.products.append(product)
 
+    
+    def SumValue(self):
+        total = 0
+        for item in self.products:
+            total += item.price * item.soh
+        
+        return total
 
-apple = Product(2, "000APL", 21)
-banana = Product(3, "000BNN", 6)
-cucumber = Product(1.5, "000CCB", 28)
 
-groceries = Inventory()
+# Example products
+apple = Product("Apple", 2, "000APL", 21)
+banana = Product("Banana", 3, "000BNN", 6)
+cucumber = Product("Cucumber", 1.5, "000CCB", 28)
+
+digitalWatch = Product("Digital watch", 120, "000DWT", 4)
+electricClock = Product("Electric clock", 20, "000ECL", 5)
+
+# Example inventory
+groceries = Inventory("Grocery section")
 groceries.AddProduct(apple)
+groceries.AddProduct(banana)
+groceries.AddProduct(cucumber)
 
-print(groceries.products[0].soh)
+electronics = Inventory("Electronics section")
+electronics.AddProduct(digitalWatch)
+electronics.AddProduct(electricClock)
+
+
+# Testing
+print(apple)
+print(groceries)
+print(groceries.SumValue())
+print(electronics)
+print(electronics.SumValue())
